4894d52e8d357243533614cbb17817372b98e602
lxfariba <f.karvand@gmail.com>
2022-08-15 17:54:40 +0430

    first commit and put models

---
+++ wallet/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)  # TODO: do not save password
+
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __unicode__(self):
+        return "{}_token".format(self.user)
+
+
+class Expense(models.Model):
+	text = models.CharField(max_length=150)
+	date = models.DateTimeField()
+	amount = models.BigIntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "{} at {}".format(self.amount,self.date)   
+
+class Income(models.Model):
+	text = models.CharField(max_length=150)
+	date = models.DateTimeField()
+	amount = models.BigIntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "{} at {}".format(self.amount,self.date)   
+
+
---
+++ wallet/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import submit_expense, submit_income, register
+
+
+app_name = "wallet"
+
+urlpatterns = [
+
+	path("submit/expense", submit_expense, name="submit_expense"),
+	path("submit/income/", submit_income, name='submit_income'),
+	path("accounts/register/", register, name='register'),
+	# path("", views.index, name='index'),
+
+	]
